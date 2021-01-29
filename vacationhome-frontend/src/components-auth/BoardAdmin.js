1896d36a717140967b02b4f5afd98417190b5e18
lara-ai-dev <lara.nettesheim@gmail.com>
2021-01-29 20:37:02 +0100

    created a table in the front end Cors was adjusted

--- vacationhome-frontend/src/components-auth/BoardAdmin.js
+++ vacationhome-frontend/src/components-auth/BoardAdmin.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import axios from "axios";
 import UserService from "../services/user.service";
+import Title from "../components/Title";
+import Table from 'react-bootstrap/Table'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class BoardAdmin extends Component {
     constructor(props) {
@@ -37,29 +40,55 @@ export default class BoardAdmin extends Component {
 
         axios.get(`/reservation`)
             .then(res => {
+
                 const reservations = res.data;
                 this.setState({reservations});
                 console.log(reservations);
             })
 
-
-
-
     }
 
     render() {
+        const {reservations} = this.state;
         return (
             <div className="reservationinformation">
+                <Title title="Reservation information"/>
             <div className="container-reservationinformation">
                 <header className="jumbotron">
                     <h3>{this.state.content}</h3>
                 </header>
                 <ul>
-                    {this.state.reservations.map(reservation => {
-                        <li>{reservation}</li>
-                    })}
+                    {this.state.reservations.map((reservation =>
+                            <li>{reservation.price}</li>
+                    ))}
+
+
                 </ul>
             </div>
+
+                <Table striped bordered hover>
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Email</th>
+                        <th>Check In</th>
+                        <th>Check Out</th>
+                        <th>Price</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {reservations.map((reservation) => {
+                        return (
+                            <tr key={reservation.id}>
+                                <td>{reservation.id}</td>
+                                <td>{reservation.price}</td>
+                            </tr>
+                        )
+                    })}
+
+                    </tbody>
+                </Table>
+
             </div>
         );
     }
