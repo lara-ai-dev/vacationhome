b42663539125826b1785581448e0889cfdcdcbc2
lara-ai-dev <lara.nettesheim@gmail.com>
2021-02-05 22:54:17 +0100

    home page can now display the reviews with the comment, rating and username

--- vacationhome-frontend/src/pages/Home.js
+++ vacationhome-frontend/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect, useCallback} from "react";
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import {Link} from 'react-router-dom';
@@ -6,10 +6,12 @@ import Services from '../components/Services';
 import FeaturedRooms from '../components/FeaturedRooms';
 import Title from "../components/Title";
 import UserService from '../services/user.service';
+import axios from 'axios';
 
 export default function Home() {
 
     const [content, setContent] = useState("");
+    const [reviews, setReviews]  = useState([]);
 
     useEffect(() => {
         UserService.getPublicContent().then(
@@ -26,6 +28,18 @@ export default function Home() {
             }
         );
     }, []);
+
+    const loadReviews = useCallback(() => {
+        axios.get(`/review`)
+            .then((response) => {
+                setReviews(response.data);
+            })
+    }, []);
+
+    //the reviews are loaded initially
+    useEffect(() => {
+        loadReviews();
+    }, [loadReviews])
 
     return (
         <>
@@ -47,7 +61,17 @@ export default function Home() {
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2546.8056335945616!2d7.220488215727086!3d50.33287737945927!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47be5ba7d0c092a1%3A0x964f96b779a765ad!2sAn%20der%20Bleiche%2C%2056727%20Mayen%2C%20Duitsland!5e0!3m2!1snl!2snl!4v1612106416181!5m2!1snl!2snl"
                     ></iframe>
             </div>
-        
+        <div>
+            <ul>
+                {reviews.map((item,i) => (
+                    <li key={i}>
+                        <p>{item.comment}</p>
+                        <p>{item.userName}</p>
+                        <p>{item.rating}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
         </>
 
 
