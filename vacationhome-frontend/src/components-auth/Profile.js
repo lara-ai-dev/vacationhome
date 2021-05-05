b08f6a2660dba255fbc8a060c6e6fba29adff97d
Lara Nettesheim <lara.nettesheim@gmail.com>
2021-05-05 12:49:31 +0200

    add reservation : post request does not show errors anymore but does not send any data yet ...

--- vacationhome-frontend/src/components-auth/Profile.js
+++ vacationhome-frontend/src/components-auth/Profile.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react'
+import React, {useContext, useState, Component} from 'react'
 import { RoomContext } from '../context'
 import AuthService from '../services/auth.service';
 import axios from "axios";
@@ -11,10 +11,12 @@ const Profile = () => {
 
     const currentUser = AuthService.getCurrentUser();
     const context = useContext(RoomContext)
-    const {
-
-    }  = context;
-
+    const {}  = context;
+    const [apartmentId, setApartmentId] = useState();
+    const [email, setEmail] = useState();
+    const [checkInDate, setCheckInDate] = useState();
+    const [checkOutDate, setCheckOutDate] = useState();
+    const [numberOfGuests, setNumberOfGuests] = useState();
 
     const apartments = useState;
 
@@ -24,21 +26,23 @@ const Profile = () => {
             console.log(apartments)
         })
 
-    /*
 
-    const handleSubmit = () => {
-        const onSubmit = useCallback(formData =>{
-            console.log(formData);
-            axios.post(`/reservation`){
-                apartmentId: this.state.apartmentNumber,
+    const handleSubmit = async (event) =>{
+        event.preventDefault();
+       // const { apartmentId,email, checkInDate, checkOutDate} = values;
 
+        await axios.post ('/reservation', {
+            
+            email,
+            checkInDate,
+            checkOutDate,
+            numberOfGuests})
+
+            .then(response =>{
+                console.log(response);
             })
-                .then(res => {
-                    console.log(res);
-                    console.log(res.data);
-                })
-        }
-    }*/
+
+        };
 
 
 
@@ -67,38 +71,48 @@ const Profile = () => {
         </div>
         </div>
 
+            <div className="reservationinformation">
+                <Title title="Make reservation"/>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-control">
+                        <label htmlFor="emailAdress">Email address</label>
+                        <input type="email" className="form-control" id="email"
+                               placeholder={currentUser.email} value={email}/>
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="apartmentSelect">Select Apartment</label>
+                        <select className="form-control" id="apartmentId" value={apartmentId} >
+                            <option>{apartments[0]}1</option>
+                            <option>{apartments[1]}2</option>
+                            <option>{apartments[2]}3</option>
+                        </select>
+                    </div>
 
-        <div className="reservationinformation">
-            <Title title="Make reservation"/>
-            <form>
-                <div className="form-control">
-                    <label htmlFor="emailAdress">Email address</label>
-                    <input type="email" className="form-control" id="emailAdress"
-                           placeholder={currentUser.email}/>
-                </div>
-                <div className="form-control">
-                    <label htmlFor="apartmentSelect">Select Apartment</label>
-                    <select className="form-control" id="apartmentSelect" value={this.state.apartmentId}>
-                        <option>{apartments[0]}1</option>
-                        <option>{apartments[1]}2</option>
-                        <option>{apartments[2]}3</option>
-                    </select>
-                </div>
-                <div className="form-control">
-                    <p>Checkin date</p>
-                    <input className="form-control" type="date" value="DD-MM-YY" id="example-date-input" {this.state.startDate}/>
-                    <p>Checkout date</p>
-                    <input className="form-control" type="date" value="DD-MM-YY" id="example-date-input" {this.state.startDate}/>
-                </div>
-                <div className="form-control">
-                <Button className="form-control" type="submit"  onPress ={handleSubmit(onSubmit)}value="Reserve"/>
-                </div>
-            </form>
+                    <div className="form-control">
+                        <p>Checkin date</p>
+                        <input className="form-control" type="date" id="example-date-input" value={checkInDate}/>
+                        <p>Checkout date</p>
+                        <input className="form-control" type="date"  id="example-date-input" value={checkOutDate}/>
+                    </div>
+
+                    <div className="form-control">
+                        <p>Number of Guests</p>
+                        <input type="number" id="noGuests" name="numberOfGuests"  min="1" max="5" value={numberOfGuests}/>
+                    </div>
 
 
-        </div>
+                    <div className="form-control">
+                        <Button className="form-control" type="submit" onSubmit="submit">Reserve</Button>
+                    </div>
+                </form>
+
+
+            </div>
+
         </>
     );
 };
+
+
 
 export default Profile;
